81992a5122943d32fa38b0bad1fd8d9c8808d3de
KristianBlom <blom.kristian99@gmail.com>
2022-11-30 17:00:13 +0100

    StringSearch implemented

--- StringSearch.java
+++ StringSearch.java
@@ -1,1 +1,24 @@
+import java.util.*;
+import java.io.*;
 
+public class StringSearch {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Wrong number of arguments.");
+            return;
+        }
+        File file = new File(args[1]);
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                if (line.contains(args[0])) {
+                    System.out.println(line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+}
